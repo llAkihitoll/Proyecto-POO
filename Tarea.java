8339c133fa62b3a39e2edad66b2882435b97c711
JAVIERCLO <118774756+JAVIERCL0@users.noreply.github.com>
2024-09-19 19:26:22 -0600

    Atributos y creador de clase Tarea

--- Tarea.java
+++ Tarea.java
@@ -1,7 +1,25 @@
 
 //Clase que representa las tareas asignadas del usuario.
-public class Tarea {
-    //Atributos
 
-    //metodos
+import java.util.ArrayList;
+
+public class Tarea {
+//Atributos
+    private String nombreTarea;     //El nombre de la tarea
+    private int prioridad;          //la prioridad de la tarea
+    private double duracion;        //la duracion de la tarea en minutos
+    private boolean completado;     //Si es verdadero la tarea está completada, si es falso no está completada
+    private String detalles;        //Descripción o comentarios sobre la tarea.
+    
+//Constructor para Tarea
+    public Tarea(String nombreTarea, int prioridad,  double duracion, boolean completado, String detalles) {
+
+        this.nombreTarea = nombreTarea;
+        this.prioridad = prioridad;
+        this.duracion = duracion;
+        this.completado = completado;
+        this.detalles = detalles;
+    }
+//metodos
+
 }
