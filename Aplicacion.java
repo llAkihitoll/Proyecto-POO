0d5578ac8dab5ac7f4d894378f980f32c34ba157
RodrigoAar <ajmacrodrigo@gmail.com>
2024-09-19 21:19:27 -0600

    Create Aplicacion.java
    Crear la base de la clase Aplicación que será el controlador del programa. Los métodos están sin parámetros y sin función (este es solo es un esqueleto).

---
+++ Aplicacion.java
@@ -0,0 +1,48 @@
+public class Aplicacion {
+    Usuario usuarioactual;
+    Horario horario;
+    Pomodoro pomodoro;
+
+    public Aplicacion(){
+
+    }
+
+    public void IniciarPomodoro(){
+        pomodoro = new Pomodoro();
+
+    }
+
+    public void IniciarDescanso(){
+
+    }
+
+    public void AddTarea(){
+
+    }
+
+    public void ReiniciarPomodoro(){
+
+    }
+
+    public void MarcarCompletado(){
+
+    }
+
+    public String obtenerDetalles(){
+
+    }
+
+    public Horario MostrarHorario(){
+
+    }
+
+    public void VerificarRecompensa(){
+
+    }
+
+    public Badeges MostrarRecompensa(){
+
+    }
+
+    
+}
