33e59ec3d27196dab0f1fb4d2efcbdc8c708a667
llAkihitoll <saul.esteban.casti@gmail.com>
2024-09-19 20:31:03 -0600

    Agregue algunos atributos

--- Pomodoro.java
+++ Pomodoro.java
@@ -1,4 +1,30 @@
+
 public class Pomodoro {
+    private int intervaloTrabajo;
+    private int intervaloDescanso;
 
+    public Pomodoro(int intervaloTrabajo, int intervaloDescansoo) {
+        this.intervaloTrabajo = intervaloTrabajo;
+        this.intervaloDescanso = intervaloDescanso; 
+    }
+    public Pomodoro() {
+        this(25, "5"); 
+    }
+
+    public String getIntervaloTrabajo() {
+        return intervaloTrabajo; 
+    }
+
+    public void setIntervaloTrabajo(String intervaloTrabajo) {
+        this.intervaloTrabajo = intervaloTrabajo; 
+    }
+
+    public int getIntervaloDescanso() {
+        return intervaloDescanso;
+    }
+
+    public void setIntervaloDescanso(int intervaloDescanso) {
+        this.intervaloDescanso = intervaloDescanso;
+    
+    } 
 }
-
