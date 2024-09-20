bafb2b2dc6b61a4e3ba7cfb49ca40732407e26a5
llAkihitoll <saul.esteban.casti@gmail.com>
2024-09-19 19:47:49 -0600

    Agregue atributos y metodos

--- Badges.java
+++ Badges.java
@@ -1,4 +1,21 @@
-public class Badges {
- 
-}
+import java.util.ArrayList;
+import java.util.List;
 
+public class Badges {
+    private List<String> recompensas;
+    }
+
+    
+    public void getRecompensas() {
+        return recompensas;  
+    }
+
+    agregarRecompensa(String recompensa) {
+        recompensas.add(recompensa);  
+
+
+    public static void mostrarRecompensas() {
+        for (String recompensa : recompensas) {  
+        }
+    }
+}
