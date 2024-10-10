4e9582e66641896bd141bc19450b106dfc2d4437
RodrigoAar <ajmacrodrigo@gmail.com>
2024-10-10 09:24:18 -0600

    Update Vista.java
    ejemplo de como crear una ventana usando swing y con herencia

--- Vista.java
+++ Vista.java
@@ -1,3 +1,19 @@
-public class Vista {
-    
+import javax.swing.*;
+public class Vista extends JFrame{
+    //ejemplo basico de un gui utilizando swing
+    //probando con herencia
+
+    public Vista(){
+        super("ejemplo");
+        //titulo con el que aparecerá la ventana
+
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //se establece que se vea la ventana y que cuando esta se cierre se terminé el programa
+
+    }
+
+    public static void main(String[] args) {
+        Vista prueba = new Vista();
+    }
 }
