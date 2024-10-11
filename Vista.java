b2b748633fcf12a674e570e1df6d92aee1b41450
RodrigoAar <ajmacrodrigo@gmail.com>
2024-10-11 08:38:59 -0600

    boton medallas
    Se agregó el botón para desplegar las medallas, del usuario, es una idea muy básica.

--- Vista.java
+++ Vista.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
+
 public class Vista  extends JFrame{
     public Vista(){
         setTitle("App manejo del tiempo");
@@ -8,18 +10,19 @@ public class Vista  extends JFrame{
         setVisible(true);
         //setSize(500, 200);
 
-       // JPanel dias = new JPanel();
-       // dias.setLayout(new GridLayout(4, 7));
-       // for(int i = 0; i<28; i++){
-       //     JButton botondia = new JButton("Día " + (i+1));
-        //    dias.add(botondia);
-       // }
+        JPanel dias = new JPanel();
+        dias.setLayout(new GridLayout(4, 7));
+        for(int i = 0; i<28; i++){
+            JButton botondia = new JButton("Día " + (i+1));
+            dias.add(botondia);
+        }
 
-        //dias.setSize(250, 230);
-        //add(dias,BorderLayout.CENTER);
+        dias.setSize(250, 230);
+        add(dias,BorderLayout.CENTER);
 
         JPanel medalla = new JPanel();
         JButton medallas = new JButton("Medallas");
+        medallas.addActionListener(new aparecer());
         medalla.add(medallas);
 
         String[] pruebalista = {"hola", "soy", "rodrigo"} ;
@@ -28,11 +31,12 @@ public class Vista  extends JFrame{
         lista.add(tareas, BorderLayout.NORTH);
         lista.add(medallas, BorderLayout.CENTER);
         add(lista, BorderLayout.EAST);
+    }
 
-        
-
-        
-
+    class aparecer implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Aquí se desplegarán las medallas con las que cuenta el usario");
+            }
     }
 
     public static void main(String[] args) {
