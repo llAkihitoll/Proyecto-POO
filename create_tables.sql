f476bffb2f2c6d862becf53f452f2c6a25d296b7
JAVIERCLO <cha23132@uvg.edu.gt>
2024-10-15 20:47:05 -0600

    Creacion de tabla pomodoro

--- create_tables.sql
+++ create_tables.sql
@@ -22,3 +22,17 @@ CREATE TABLE tareas (
     fecha_fin DATETIME,                                                             -- Fecha y hora de finalizacion de la tarea
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE
 );
+
+/*
+Tabla para almacenar las sesiones del metodo pomodoro de las tareas.
+*/
+CREATE TABLE pomodoro (
+    id INT AUTO_INCREMENT PRIMARY KEY,  -- Identificador único para cada sesión Pomodoro
+    id_tarea INT NOT NULL,              -- Llave foranea que referencia a la tarea
+    duracion INT NOT NULL,              -- Duración de la sesion en minutos
+    descanso INT NOT NULL,              -- Duracion del descanso en minutos
+    sesion_actual INT NOT NULL,         -- Numero de la sesión actual
+    fecha_inicio DATETIME NOT NULL,     -- Fecha y hora de inicio de la sesion
+    FOREIGN KEY (id_tarea) REFERENCES tareas(id) ON DELETE CASCADE
+);
+
