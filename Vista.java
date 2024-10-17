36450746df2ed49a999bef4f995ae151a0fa4974
RodrigoAar <ajmacrodrigo@gmail.com>
2024-10-17 09:14:15 -0600

    Update Vista.java
    Agregar la ventana de interacción con el usuario cuando se inicie el programa y crear el método para iniciar sesión cuando se oprima ese boton

--- Vista.java
+++ Vista.java
@@ -4,39 +4,58 @@ import java.awt.event.*;
 
 public class Vista  extends JFrame{
     public Vista(){
-        setTitle("App manejo del tiempo");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
-        setVisible(true);
-        //setSize(500, 200);
+        /**
+         * Crear la ventana que se desplegará cuando el ususario inicie la aplicación
+         * En esta se le preguntará si desea registrarse o iniciar sesión
+         */
+        JFrame ingreso = new JFrame();
+        ingreso.setTitle("App manejo de tiempo");
+        ingreso.setVisible(true);
+        ingreso.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ingreso.setSize(400,300);
 
-        JPanel dias = new JPanel();
-        dias.setLayout(new GridLayout(4, 7));
-        for(int i = 0; i<28; i++){
-            JButton botondia = new JButton("Día " + (i+1));
-            dias.add(botondia);
-        }
+        /**
+         * Crear un nuevo panel para poder agregar el texto para comunicarse con el usuario y los botones para que decida que hacer
+         */
+        JPanel interaccion_ingreso = new JPanel();
+        interaccion_ingreso.setLayout(new GridLayout(3,1)); 
 
-        dias.setSize(250, 230);
-        add(dias,BorderLayout.CENTER);
+        /**
+         * Crear la etiqueta de texto para informarle al usuario que esta es la ventana de ingreso
+         */
 
-        JPanel medalla = new JPanel();
-        JButton medallas = new JButton("Medallas");
-        medallas.addActionListener(new aparecer());
-        medalla.add(medallas);
+         JLabel info = new JLabel("Bienvenido al programa de manejo de tiempo");
 
-        String[] pruebalista = {"hola", "soy", "rodrigo"} ;
-        JPanel lista = new JPanel();
-        JList<String> tareas = new JList<>(pruebalista);
-        lista.add(tareas, BorderLayout.NORTH);
-        lista.add(medallas, BorderLayout.CENTER);
-        add(lista, BorderLayout.EAST);
+         /**
+          * Crear botones para que el usuario elija si desea iniciar sesión o registrarse
+          * Es decir, si ya tiene usuario o quiere crear uno
+          */
+
+          JButton iniciar_sesion = new JButton("Iniciar sesión");
+          JButton registrarse = new JButton("Registrarse");
+
+          /**
+           * Agregar las componentes al panel (importa el orden)
+           */
+           interaccion_ingreso.add(info); //se agrega a la primera fila
+           interaccion_ingreso.add(iniciar_sesion); // se agrega a la segunda fila
+           interaccion_ingreso.add(registrarse); // se agrega a la tercera fila
+
+           /**
+            * Agregar el panel la ventana
+            */
+           ingreso.add(interaccion_ingreso);
+
     }
 
-    class aparecer implements ActionListener{
-        public void actionPerformed(ActionEvent e) {
-            System.out.println("Aquí se desplegarán las medallas con las que cuenta el usario");
-            }
+    public static void ventana_inicio_sesion(){
+        JFrame inicio_de_sesion = new JFrame();
+        inicio_de_sesion.setTitle("App manejo de tiempo - Iniciar sesión");
+        inicio_de_sesion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        inicio_de_sesion.setSize(350, 250);
+        inicio_de_sesion.setVisible(true);
+
+        
     }
 
     public static void main(String[] args) {
