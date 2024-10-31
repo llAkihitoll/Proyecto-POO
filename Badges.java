797761a180b3b1887424e854d8b119d4429b8168
llAkihitoll <saul.esteban.casti@gmail.com>
2024-10-31 15:30:11 -0600

    Update Badges.java

--- Badges.java
+++ Badges.java
@@ -4,10 +4,13 @@ import java.util.List;
 // lista privada que almacena las recompensas
 public class Badges {
     private List<String> recompensas;
+    private List<String> recompensasAdquiridas;
 
 // constructor que inicializa la lista de recompensas como un nuevo Arraylist
     public Badges() {
         this.recompensas = new ArrayList<>();
+        this.recompensasAdquiridas = new ArrayList<>();
+        inicializarRecompensas();
     }
     
 // metodo para que devuelva la lista de recompensas
